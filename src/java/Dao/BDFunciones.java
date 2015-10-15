ad02e0dc86430f25a75964a739310a579d861eb0
Jorge <jorgeluis.tanos@exitco.biz>
2015-10-14 21:14:52 -0300

    ABM Funciones

--- src/java/Dao/BDFunciones.java
+++ src/java/Dao/BDFunciones.java
@@ -5,7 +5,9 @@
  */
 package Dao;
 
+import Modelo.Cine;
 import Modelo.Funcion;
+import Modelo.Pelicula;
 import Modelo.Sala;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -79,18 +81,20 @@ public class BDFunciones implements IBD{
         Funcion resp = null;
         Sala sala = null;
         Cine cine = null;
+        Pelicula peli = null;
         Conexion oCon = new Conexion();
         ResultSet rs = null;       
         oCon.getConexion();
         Funcion fun = (Funcion)dato;
-        String consulta = "SELECT * FROM funciones inner join salas on idSala =" +fun.getSala().getIdSala()+" inner join peliculas on idPelicula="+fun.getPelicula().getIdPelicula() + "where idSala ="+fun.getSala().getIdSala() + "and idPelicula ="+fun.getPelicula().getIdPelicula() +" and FechaYHora =" + fun.getFechaYHora();      
+        String consulta = "SELECT * FROM funciones inner join cines on idCine="+fun.getSala().getCine().getIdCine()+" inner join salas on idSala =" +fun.getSala().getIdSala()+" inner join peliculas on idPelicula="+fun.getPelicula().getIdPelicula() + "where idSala ="+fun.getSala().getIdSala() + "and idPelicula ="+fun.getPelicula().getIdPelicula() +" and FechaYHora =" + fun.getFechaYHora();      
         try {
             PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(consulta);
             rs = sentencia.executeQuery();            
             while (rs.next()) {
-                cine = new Cine();
+                peli = new Pelicula(rs.getInt("idPelicula"),rs.getString("Nombre"),rs.getString("Director"),rs.getInt("Duracion"),rs.getString("Descripcion"),rs.getBoolean("Estado"),rs.getString("UrlImagen"));
+                cine = new Cine(rs.getInt("idCine"),rs.getString("Nombre"),rs.getString("Direccion"),rs.getBoolean("Estado"));
                 sala = new Sala(rs.getInt("idSala"),rs.getInt("NumSala"),cine,rs.getInt("Columna"),rs.getInt("Fila"),rs.getBoolean("Estado"));
-                resp = new Funcion(rs.getDate("FechaYHora"), rs.getInt("Duracion"), rs.getFloat("Precio"),sala, rs.getString("Descripcion"), rs.getBoolean("Estado"), rs.getString("UrlImagen"));
+                resp = new Funcion(rs.getDate("FechaYHora"), rs.getInt("Duracion"), rs.getFloat("Precio"),sala, peli);
             }
             rs.close();
             sentencia.close();
