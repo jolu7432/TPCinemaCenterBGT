d76dad46b8b27b59117ddc214795da931edd7cfb
valtanders <brunobollati@gmail.com>
2015-10-21 21:11:42 -0300

    Merge origin/master
    Conflicts:
    	web/listadoFunciones.jsp
    	web/menu.jsp

--- src/java/Dao/BDUsuarios.java
+++ src/java/Dao/BDUsuarios.java
@@ -20,10 +20,10 @@ public class BDUsuarios implements IBD {
     private static BDUsuarios instance = null;
 
     public static BDUsuarios getInstance() {
-        if (instance == null) {
-            instance = new BDUsuarios();
-        }
-        return instance;
+	if (instance == null) {
+	    instance = new BDUsuarios();
+	}
+	return instance;
     }
 
     public BDUsuarios() {
@@ -32,124 +32,144 @@ public class BDUsuarios implements IBD {
 
     @Override
     public void alta(Object dato) throws SQLException {
-        Conexion oCon = new Conexion();
-        oCon.getConexion();
-        Usuario aux = (Usuario) dato;
-        String insert = "INSERT INTO usuarios(nombre, apellido, DNI, Administrador, User, Pass, email,Telefono,UrlImg) VALUES('" +aux.getNombre()+ "','" +aux.getApellido()+ "'," +aux.getDni()+ "," +aux.isAdministrador()+ ",'" +aux.getUser()+ "','" +aux.getPass()+ "','"+aux.getEmail()+"','"+aux.getTelefono()+ "','"+aux.getUrlImg()+"')";
-        try {
-            PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(insert);
-            sentencia.execute();
-            sentencia.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            oCon.close();
-        }
+	Conexion oCon = new Conexion();
+	oCon.getConexion();
+	Usuario aux = (Usuario) dato;
+	String insert = "INSERT INTO usuarios(nombre, apellido, DNI, Administrador, User, Pass, email,Telefono,UrlImg) VALUES('" + aux.getNombre() + "','" + aux.getApellido() + "'," + aux.getDni() + "," + aux.isAdministrador() + ",'" + aux.getUser() + "','" + aux.getPass() + "','" + aux.getEmail() + "','" + aux.getTelefono() + "','" + aux.getUrlImg() + "')";
+	try {
+	    PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(insert);
+	    sentencia.execute();
+	    sentencia.close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    oCon.close();
+	}
     }
 
     @Override
     public void baja(Object dato) throws SQLException {
-        Conexion oCon = new Conexion();
-        oCon.getConexion();
-        Usuario aux = (Usuario) dato;
-        String borrar = "DELETE from usuarios WHERE idUsuario = " + aux.getId();
-        try{
-            PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(borrar);
-            sentencia.execute();
-            sentencia.close();
-        } catch (SQLException e){
-            e.printStackTrace();
-        } finally {
-            oCon.close();
-        }
+	Conexion oCon = new Conexion();
+	oCon.getConexion();
+	Usuario aux = (Usuario) dato;
+	String borrar = "DELETE from usuarios WHERE idUsuario = " + aux.getId();
+	try {
+	    PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(borrar);
+	    sentencia.execute();
+	    sentencia.close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    oCon.close();
+	}
     }
-    
 
     @Override
     public void modificar(Object dato) throws SQLException {
-        Conexion oCon = new Conexion();
-        oCon.getConexion();
-        Usuario aux = (Usuario) dato;
-        String update = "UPDATE usuarios SET  nombre = '" +aux.getNombre()+ "', apellido = '" +aux.getApellido()+ "', DNI = " +aux.getDni()+ ", Administrador = " +aux.isAdministrador()+ ", User = '" +aux.getUser()+ "', Pass = '" +aux.getPass()+ "', email = '"+aux.getEmail()+"', Telefono = '"+aux.getTelefono()+ "'  WHERE idUsuario = " + aux.getId();
-        try {
-            PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(update);
-            sentencia.execute();
-            sentencia.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            oCon.close();
-        }
+	Conexion oCon = new Conexion();
+	oCon.getConexion();
+	Usuario aux = (Usuario) dato;
+	String update = "UPDATE usuarios SET  nombre = '" + aux.getNombre() + "', apellido = '" + aux.getApellido() + "', DNI = " + aux.getDni() + ", Administrador = " + aux.isAdministrador() + ", User = '" + aux.getUser() + "', Pass = '" + aux.getPass() + "', email = '" + aux.getEmail() + "', Telefono = '" + aux.getTelefono() + "'  WHERE idUsuario = " + aux.getId();
+	try {
+	    PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(update);
+	    sentencia.execute();
+	    sentencia.close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    oCon.close();
+	}
     }
 
     @Override
     public Object existe(Object dato) throws SQLException {
-        Usuario resp = null;
-        Conexion oCon = new Conexion();
-        ResultSet rs = null;
-        oCon.getConexion();
-        String insert = "SELECT * FROM usuarios where User='"+((Usuario)dato).getUser() + "' and Pass='"+((Usuario)dato).getPass()+"'";      
-        try {
-            PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(insert);
-            rs = sentencia.executeQuery();            
-            while (rs.next()) {
-                resp = new Usuario(rs.getInt("idUsuario"), rs.getString("Nombre"), rs.getString("Apellido"), rs.getInt("DNI"),rs.getBoolean("Administrador"), rs.getString("User"), rs.getString("Pass"), rs.getString("email"), rs.getString("Telefono"), rs.getString("UrlImg"));
-            }
-            rs.close();
-            sentencia.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            oCon.close();
-            return resp;
-        }
+	Usuario resp = null;
+	Conexion oCon = new Conexion();
+	ResultSet rs = null;
+	oCon.getConexion();
+	String insert = "SELECT * FROM usuarios where User='" + ((Usuario) dato).getUser() + "' and Pass='" + ((Usuario) dato).getPass() + "'";
+	try {
+	    PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(insert);
+	    rs = sentencia.executeQuery();
+	    while (rs.next()) {
+		resp = new Usuario(rs.getInt("idUsuario"), rs.getString("Nombre"), rs.getString("Apellido"), rs.getInt("DNI"), rs.getBoolean("Administrador"), rs.getString("User"), rs.getString("Pass"), rs.getString("email"), rs.getString("Telefono"), rs.getString("UrlImg"));
+	    }
+	    rs.close();
+	    sentencia.close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    oCon.close();
+	    return resp;
+	}
     }
 
     @Override
     public ArrayList listado() throws SQLException {
-        Usuario resp = null;
-        Conexion oCon = new Conexion();
-        ResultSet rs = null;
-        ArrayList listaUsuarios = new ArrayList();
-        oCon.getConexion();
-        String consulta = "SELECT * FROM usuarios";      
-        try {
-            PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(consulta);
-            rs = sentencia.executeQuery();            
-            while (rs.next()) {
-                resp = new Usuario(rs.getInt("idUsuario"), rs.getString("Nombre"), rs.getString("Apellido"), rs.getInt("DNI"), rs.getBoolean("Administrador"), rs.getString("User"), rs.getString("Pass"), rs.getString("email"), rs.getString("Telefono"), rs.getString("UrlImg"));
-                listaUsuarios.add(resp);
-            }
-            rs.close();
-            sentencia.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            oCon.close();
-            return listaUsuarios;
-        }
+	Usuario resp = null;
+	Conexion oCon = new Conexion();
+	ResultSet rs = null;
+	ArrayList listaUsuarios = new ArrayList();
+	oCon.getConexion();
+	String consulta = "SELECT * FROM usuarios";
+	try {
+	    PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(consulta);
+	    rs = sentencia.executeQuery();
+	    while (rs.next()) {
+		resp = new Usuario(rs.getInt("idUsuario"), rs.getString("Nombre"), rs.getString("Apellido"), rs.getInt("DNI"), rs.getBoolean("Administrador"), rs.getString("User"), rs.getString("Pass"), rs.getString("email"), rs.getString("Telefono"), rs.getString("UrlImg"));
+		listaUsuarios.add(resp);
+	    }
+	    rs.close();
+	    sentencia.close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    oCon.close();
+	    return listaUsuarios;
+	}
     }
-    
-    public Usuario traePorId(int id) throws SQLException{
-        Usuario resp = null;
-        Conexion oCon = new Conexion();
-        ResultSet rs = null;
-        oCon.getConexion();
-        String consulta = "SELECT * FROM usuarios where idUsuario = " + id;
-        try { 
-            PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(consulta);
-            rs = sentencia.executeQuery();
-            while(rs.next()){
-                resp = new Usuario(rs.getInt("idUsuario"), rs.getString("Nombre"), rs.getString("Apellido"), rs.getInt("DNI"), rs.getBoolean("Administrador"), rs.getString("User"), rs.getString("Pass"), rs.getString("email"), rs.getString("Telefono"), rs.getString("UrlImg"));
-            }
-            rs.close();
-            sentencia.close();
-        } catch (SQLException e){
-            e.printStackTrace();
-        } finally {
-            oCon.close();
-            return resp;
-        }
+
+    public Usuario traePorId(int id) throws SQLException {
+	Usuario resp = null;
+	Conexion oCon = new Conexion();
+	ResultSet rs = null;
+	oCon.getConexion();
+	String consulta = "SELECT * FROM usuarios where idUsuario = " + id;
+	try {
+	    PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(consulta);
+	    rs = sentencia.executeQuery();
+	    while (rs.next()) {
+		resp = new Usuario(rs.getInt("idUsuario"), rs.getString("Nombre"), rs.getString("Apellido"), rs.getInt("DNI"), rs.getBoolean("Administrador"), rs.getString("User"), rs.getString("Pass"), rs.getString("email"), rs.getString("Telefono"), rs.getString("UrlImg"));
+	    }
+	    rs.close();
+	    sentencia.close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    oCon.close();
+	    return resp;
+	}
+    }
+
+    public Usuario validaUser(String user, String email) throws SQLException {
+	Usuario resp = null;
+	Conexion oCon = new Conexion();
+	ResultSet rs = null;
+	String consulta = "SELECT * FROM usuarios where User = '" + user + "' and email = '" + email + "'";
+	try {
+	    PreparedStatement sentencia = (PreparedStatement) oCon.getConexion().prepareStatement(consulta);
+	    rs = sentencia.executeQuery();
+	    while (rs.next()) {
+		resp = new Usuario(rs.getInt("idUsuario"), rs.getString("Nombre"), rs.getString("Apellido"), rs.getInt("DNI"), rs.getBoolean("Administrador"), rs.getString("User"), rs.getString("Pass"), rs.getString("email"), rs.getString("Telefono"), rs.getString("UrlImg"));
+	    }
+	    rs.close();
+	    sentencia.close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    oCon.close();
+	    return resp;
+	}
     }
 
 }
